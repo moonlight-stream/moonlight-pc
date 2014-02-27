92b2c2a1561d7de8c1afc71003cdd66a539da26e
Cameron Gutman <aicommander@gmail.com>
2014-02-26 19:00:30 -0500

    Don't send paint messages to the window since we're using active rendering

--- src/com/limelight/gui/StreamFrame.java
+++ src/com/limelight/gui/StreamFrame.java
@@ -102,6 +102,8 @@ public class StreamFrame extends JFrame {
 		
 		this.addWindowListener(createWindowListener());
 		
+		this.setIgnoreRepaint(true);
+		
 		if (fullscreen) {
 			makeFullScreen(streamConfig);
 			
