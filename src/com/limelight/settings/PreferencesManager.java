497051d355fda9d6f4611c0dcb93b0b782de7594
Aaron Neyer <aaronneyer@gmail.com>
2014-07-31 18:30:46 -0700

    add 1366x768 and 1600x900 as optional resolutions

--- src/com/limelight/settings/PreferencesManager.java
+++ src/com/limelight/settings/PreferencesManager.java
@@ -64,6 +64,8 @@ public abstract class PreferencesManager {
 		 * The possible resolutions for the stream
 		 */
 		public enum Resolution { RES_720_30(1280, 720, 30, 5), RES_720_60(1280, 720, 60, 10), 
+			RES_768_30(1366, 768, 30, 5), RES_768_60(1366, 768, 60, 15),
+			RES_900_30(1600, 900, 30, 10), RES_900_60(1600, 900, 60, 20),
 			RES_1080_30(1920, 1080, 30, 10), RES_1080_60(1920, 1080, 60, 25);
 			public int width;
 			public int height;
@@ -87,6 +89,15 @@ public abstract class PreferencesManager {
 			@Override
 			public String toString() {
 				return String.format("%dx%d (%dHz)", width, height, frameRate);
+			}
+			
+			public static Resolution findRes(int height, int refresh) {
+			    for (Resolution res : Resolution.values()) {
+			        if (res.height == height && res.frameRate == refresh) {
+			            return res;
+			        }
+			    }
+			    return null;
 			}
 		};
 		
