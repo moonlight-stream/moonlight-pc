50ebf1e3b53011778824ab973a223773fad57d17
Cameron Gutman <aicommander@gmail.com>
2016-01-27 06:35:46 -0500

    Fix loading of Win32 nv_avc_dec library

--- jni/nv_avc_dec/build-win32-mingw.ps1
+++ jni/nv_avc_dec/build-win32-mingw.ps1
@@ -4,7 +4,7 @@ $env:path="$env:MINGW_32_HOME/bin;$env:path"
 rm *.o
 rm nv_avc_dec.dll
 & gcc -O2 -I $Env:JAVA_HOME/include/ -I $Env:JAVA_HOME/include/win32/ -I ./include/ -c *.c
-& gcc -shared -o nv_avc_dec.dll *.o -L./lib_win32 -lavcodec -lavutil -lswresample -lswscale -lpthreadVC2
+& gcc -shared -o nv_avc_dec.dll *.o "-Wl,--kill-at" -L./lib_win32 -lavcodec -lavutil -lswresample -lswscale -lpthreadVC2
 
 cp nv_avc_dec.dll ../../libs/win32/
 rm *.o
